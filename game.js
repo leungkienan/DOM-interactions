5422077050f09796e7b21176c12b1fbb8e42e099
Kienan Leung <leung.kienan@gmail.com>
2020-09-13 03:08:19 +1200

    writing counting function

--- game.js
+++ game.js
@@ -1,45 +1,62 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener("DOMContentLoaded", start);
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+function start() {
+  bindEventListeners(document.getElementsByClassName("board")[0].children);
 }
 
-function bindEventListeners (dots) {
+function bindEventListeners(dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener("contextmenu", makeGreen);
+    dots[i].addEventListener("click", makeBlue);
+    dots[i].addEventListener("dblclick", hide);
+
   }
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle("green");
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue(evt) {
+  evt.target.classList.toggle("blue");
+  updateCounts();
+}
 // CREATE FUNCTION hide HERE
-
-function updateCounts () {
+function hide(evt){
+  evt.target.classList.toggle('invisible');
+  updateCounts;
+}
+function updateCounts() {
   var totals = {
     blue: 0,
     green: 0,
-    invisible: 0
-  }
-  
+    invisible: 0,
+  };
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+      // checks the board for dots 
+      // if dot = blue, increase blue by one, etc. 
+      // if we find a blue dot in the bot right, we call the function
+      // update the totals[0] to increase its value by one. 
+ blue = document.getElementsByClassName(blue)
+ 
+ document.getElementsByClassName(green)
+ document.getElementsByClassName(invisible)
 
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
-function displayTotals (totals) {
+function displayTotals(totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + "-total").innerHTML = totals[key];
   }
 }
